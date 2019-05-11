492fcab4936ce04710a34d148cd83d234590db5c
Annia Forney <anniaforney@gmail.com>
2019-05-11 16:44:57 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,10 +1,8 @@
 class ChattyStudent
   attr_accessor :chatty_student
 
-def initialize(chatty_student)
-  @chatty_student = chatty_student
-end
-  def hello
-    puts "hello"
+  def initialize(chatty_student)
+    @chatty_student = chatty_student
   end
+  
 end
