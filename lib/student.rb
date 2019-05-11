2f975b28c8fe81f21849b0f6defe1154e987004a
Annia Forney <anniaforney@gmail.com>
2019-05-11 17:02:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,13 @@
-class Student 
+class Student
 
   def hello
     puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
+    super
+    raise_hand = true 
     puts "Pick me!"
+
   end
 end
